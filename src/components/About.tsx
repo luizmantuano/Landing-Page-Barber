dc1030e2df82c5d0e88332712c2ede06b402fac5
Luiz Mantuano <luguto23@hotmail.com>
2024-03-15 14:23:23 -0300

    feat:

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,14 +12,14 @@ export default function About() {
   }, []);
 
   return (
-    <section className='flex flex-1 h-[506px] bg-bg '>
-      <div className='flex flex-row '>
-        <div className='pl-[87px] mt-[-90px]' data-aos='zoom-in' data-aos-delay='100'>
-          <Image src={Images} alt='Imagem sobre a barbearia' className='object-cover' />
+    <section className='flex flex-1 h-[972px] md:h-[506px] bg-bg '>
+      <div className='flex flex-col md:flex-row'>
+        <div className=' mt-[171px] md:mt-[-90px]' data-aos='zoom-in' data-aos-delay='100'>
+          <Image src={Images} alt='Imagem sobre a barbearia' className='object-cover md:pl-[87px]' width={1000} height={529}/>
         </div>
 
         <div
-          className='flex flex-col w-[540px]  ml-[160px] mt-[48px]'
+          className='flex flex-col  ml-[160px] mt-[48px]'
           data-aos='zoom-out-left'
           data-aos-delay='250'
         >

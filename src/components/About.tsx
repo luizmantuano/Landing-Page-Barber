0e7d95a759efac32a02fd26ee23f7b4ce9dc4ef2
Luiz Mantuano <luguto23@hotmail.com>
2024-03-18 20:25:12 -0300

    feat:

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,6 +13,7 @@ export default function About() {
 
   return (
     <section className='flex flex-1 h-[972px] md:h-[506px] bg-bg '>
+
       <div className='flex flex-col md:flex-row items-center justify-center'>
         <div className=' mt-[150px] md:mt-[-90px]' data-aos='zoom-in' data-aos-delay='100'>
           <Image src={Images} alt='Imagem sobre a barbearia' className='object-cover md:pl-[87px] '/>
